973390e6f340784da3c4881be62dace591f34909
mehdinajibpour <najibpour6@gmail.com>
2024-09-04 10:00:00 +0330

    Fixes some bugs and make data source flow

---
+++ app/src/main/java/com/example/musicplayer/ui/states/PlayerState.kt
@@ -0,0 +1,22 @@
+package com.example.musicplayer.ui.states
+
+import androidx.media3.common.Player
+
+enum class PlayerState {
+    PLAYING,
+    PAUSED,
+    STOPPED;
+
+    val state: Int @Player.State get() = when (this) {
+            PLAYING -> Player.STATE_READY
+            PAUSED -> Player.STATE_READY
+            STOPPED -> Player.STATE_IDLE
+    }
+}
+
+fun Int.toPlayerState(isPlaying: Boolean) =
+    when (this) {
+        Player.STATE_IDLE -> PlayerState.STOPPED
+        Player.STATE_ENDED -> PlayerState.STOPPED
+        else -> if (isPlaying) PlayerState.PLAYING else PlayerState.PAUSED
+    }
